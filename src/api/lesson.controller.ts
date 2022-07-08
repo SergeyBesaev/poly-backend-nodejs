8d772e28d39c0935309c8f2ca0ce7e11bbe69024
sergey <besaevso@novotelecom.ru>
2022-07-08 08:22:37 +0300

    fix

--- src/api/lesson.controller.ts
+++ src/api/lesson.controller.ts
@@ -13,6 +13,16 @@ export function initApi(
             next()
         }))
 
+    app.route('/lesson-1-1')
+        .get(asyncHandler(async (req, res, next) => {
+            res.locals.body = await service.getFirstVerb(req.headers.authorization as string)
+            next()
+        }))
+        .post(asyncHandler(async (req, res, next) => {
+            res.locals.body = await service.checkInputVerb(req.headers.authorization as string, req.body.engForm as string)
+            next()
+        }))
+
     app.route('/dictionary')
         .get(asyncHandler(async (req, res, next) => {
             res.locals.body = await speechService.returnAllPartOfSpeech()
